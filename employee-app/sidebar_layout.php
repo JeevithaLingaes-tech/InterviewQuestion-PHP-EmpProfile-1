b4252b6614610a76e68b76c7f7df6a9c8f2339ab
Jeevitha Lingaes <jeevitha@radicals.io>
2025-05-21 16:23:57 +0800

    Add sidebar layout

---
+++ employee-app/sidebar_layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Sidebar Layout</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <div class="sidebar">
+    <h3>Menu</h3>
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="employees.php">Employee</a></li>
+    </ul>
+  </div>
+</body>
+
+</html>
