da321e41356ef8623e7cfb95d19a01afbe75a581
Jeevi <jeevitha130705@gmail.com>
2025-05-25 16:24:38 +0800

    Sidebar layout and style.css

--- employee-app/sidebar_layout.php
+++ employee-app/sidebar_layout.php
@@ -1,3 +1,7 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Sidebar Layout</title>
   <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
 <body>
@@ -13,8 +18,14 @@
   <div class="sidebar">
     <h3>Menu</h3>
     <ul>
-      <li><a href="home.php">Home</a></li>
-      <li><a href="employees.php">Employee</a></li>
+
+    <li><a href="home.php" class="<?= $currentPage == 'home.php' ? 'active' : '' ?>"><i class="fas fa-house sidebar-icon"></i>Home</a></li>
+    <li>
+  <a href="employees.php" class="<?= in_array($currentPage, ['employees.php', 'new_employee.php']) ? 'active' : '' ?>">
+    <i class="fas fa-users sidebar-icon"></i>Employee
+  </a>
+</li>
+
     </ul>
   </div>
 </body>
